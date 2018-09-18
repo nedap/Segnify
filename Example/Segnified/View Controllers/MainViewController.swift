71570e9dfc111a0dfc22cbec73a9fa3be3ecec7d
Bart Hopster <bart.hopster@nedap.com>
2018-09-18 14:37:09 +0200

    Added an example application (WIP), added support for Xcode 10 and Swift 4.2, updated SnapKit to 4.0.1.

---
+++ Example/Segnified/View Controllers/MainViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  Segnified
+//
+//  Created by Bart Hopster on 17/09/2018.
+//  Copyright © 2018 Bart Hopster. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+}
