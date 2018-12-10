8e8420975a765e999c83272af3c31b3b0352cdfa
Bart Hopster <bart.hopster@nedap.com>
2018-12-10 16:58:15 +0100

    Improved order of `ImageSegmentProtocol` methods.

--- Segnify/Protocols/ImageSegmentProtocol.swift
+++ Segnify/Protocols/ImageSegmentProtocol.swift
@@ -10,12 +10,12 @@ import UIKit
 
 /// Customize any `ImageSegment` appearance by implementing this protocol.
 public protocol ImageSegmentProtocol: SegmentProtocol {
-
-    /// Defines if the image of the `ImageSegment` instance should be adjusted for the different states.
-    func isAdjustingImage(for state: UIControl.State) -> Bool
     
     /// The edge insets to be applied to the image view of the `ImageSegment` instance.
     var imageViewEdgeInsets: UIEdgeInsets { get }
+    
+    /// Defines if the image of the `ImageSegment` instance should be adjusted for the different states.
+    func isAdjustingImage(for state: UIControl.State) -> Bool
     
     /// Defines the tint color of the `ImageSegment` instance.
     var tintColor: UIColor? { get }
