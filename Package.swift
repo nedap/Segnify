5b6a383d464387435978f8b04688cf5f055f2592
Manuel Martiarena <manuel.martiarena@nedap.com>
2021-05-06 16:32:30 +0200

    SEG-33: Update package ios version to 12

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "Segnify",
     defaultLocalization: "en",
     platforms: [
-        .iOS(.v10),
+        .iOS(.v12),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
